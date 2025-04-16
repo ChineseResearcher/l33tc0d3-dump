70181bacc0e0ec4031e4d6f64a14d43839b690e3
ChineseResearcher <notneasd@gmail.com>
2025-04-16 22:02:00 +0800

    Optimise Q45 Jump Game II

--- greedy/Q45 Jump Game II.py
+++ greedy/Q45 Jump Game II.py
@@ -1,31 +1,28 @@
 # greedy - medium
-class Solution():
+class Solution:
     def jump(self, nums):
-        def minMoves(nums):
-            
-            # as we are standing at pos 0, we've already reached the end
-            if len(nums) == 1:
-                return 0
-            
-            last_position = 0
-            for i in range(len(nums)):
-                # greedily update the furthest reachable position
-                last_position = max(last_position, i+nums[i])
+        n = len(nums)
+        # edge case: nums is length 1
+        if n == 1: return 0
 
-                # signal reaching the end
-                if last_position >= len(nums) - 1:
-                    cutoff_idx = i
-                    break
-            
-            # recursive call to the next sub-problem concerning the remaining length leftwards
-            # e.g. in [2,3,1,1,4]: at idx 1 with jump 3, we can reach the end
-            # the next sub-problem becomes [2,3] and at idx 0 with jump 2, we can again reach the end
-            # so the expected ans. is 2 jumps
-            return 1 + minMoves(nums[:cutoff_idx+1])
-        
-        return minMoves(nums)
-    
+        # the qn guarantees reachable to the end, so first
+        # element in nums is definitely non-zero
+        maxJump = nums[0]
+
+        # as maxJump is init. to nums[0], assume we have taken first step
+        ans = 1
+
+        if maxJump >= n-1: return ans
+        for i in range(n-1):
+            for j in range(i+1, maxJump+1):
+                maxJump = max(maxJump, j + nums[j])
+                if maxJump >= n-1:
+                    return ans + 1
+
+            ans += 1
+
 nums = [2,3,1,1,4]
 nums = [2,3,0,1,4]
+nums = [1,2]
 
 Solution().jump(nums)
