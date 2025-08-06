f91482f3ecec3fec632c53c0d7061ebe9406b621
ChineseResearcher <notneasd@gmail.com>
2025-08-06 23:07:13 +0800

    Improve Q50

--- recursion/Q50 Pow(x, n).py
+++ recursion/Q50 Pow(x, n).py
@@ -1,24 +1,34 @@
 # recursion - medium
+from functools import cache
 class Solution:
+    @cache
     def recursivePow(self, x, n):
-        intN = int(n)
-        if intN == 0:
-            return 1
-        # recursive call with power halved
-        powerHalved = self.recursivePow(x, n/2)
 
-        if intN % 2 == 0:
+        if n == 0:
+            return 1
+        
+        if n >= 0:
+            nn = n // 2
+        else:
+            nn = -(abs(n) // 2)
+
+        # recursive call with power halved
+        powerHalved = self.recursivePow(x, nn)
+
+        if abs(n) % 2 == 0:
             return powerHalved * powerHalved
         else:
             if n >= 0:
                 return powerHalved * powerHalved * x
             else:
                 # handle negative power
-                return powerHalved * powerHalved * (1/x)
+                return powerHalved * powerHalved * (1 / x)
 
     def myPow(self, x: float, n: int) -> float:
-        # thinking of this problem recursively results in O(logN) complexity
-        # this is faster than a linear product sum of x for n times
+
+        # core ideas:
+        # 1) tackling this problem recursively results in O(logN) complexity
+        # 2) as we might repeat subproblems, speed up using cache
         return self.recursivePow(x, n)
     
 x, n = 2.00000, 10
