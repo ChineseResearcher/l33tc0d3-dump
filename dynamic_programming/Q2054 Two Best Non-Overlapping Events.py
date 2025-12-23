7620b0aa817238ccdab9d17bd1c82ad566089df5
ChineseResearcher <notneasd@gmail.com>
2025-12-23 21:58:35 +0800

    Update Q2054

--- dynamic_programming/Q2054 Two Best Non-Overlapping Events.py
+++ dynamic_programming/Q2054 Two Best Non-Overlapping Events.py
@@ -1,28 +1,8 @@
 # dp - medium
-# this question is a similar to Q1235
+from typing import List
 class Solution:
-    def binarySearch(self, l, r, startTime, endTime):
-
-        # goal is the rightmost valid index such that
-        # endTime[index] <= startTime[initial r]
-        time_cutoff = startTime[r]
-
-        rightMost = -1 # dummy
-        while l <= r:
-
-            mid = (l+r) // 2
-            # print(mid)
-            if endTime[mid] <= time_cutoff:
-
-                rightMost = max(rightMost, mid)
-                l = mid+1
-
-            else:
-                r = mid-1
-
-        return rightMost
-
-    def maxTwoEvents(self, events):
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        
         n = len(events)
         # offset the startTime for each event by -1 to align with definition in the description
         for i in range(n):
@@ -39,20 +19,43 @@ class Solution:
         dp[0] = value[0]
 
         # initiate a dict tracking the value of the most valuable 
-        # event that ends before startTime[i]
-        prev_max_val = {0: value[0]}
+        # event that ends before startTime[i], using index i as the key
+        prev_max_val = [0] * n
+        prev_max_val[0] = value[0]
 
+        fmax = lambda a, b: a if a >= b else b
+
+        def bs(l, r):
+
+            # goal is the rightmost valid index such that
+            # endTime[index] <= startTime[initial r]
+            time_cutoff = startTime[r]
+
+            rightMost = -1
+            while l <= r:
+
+                mid = (l+r) // 2
+                if endTime[mid] <= time_cutoff:
+                    rightMost = fmax(rightMost, mid)
+                    l = mid+1
+                else:
+                    r = mid-1
+
+            return rightMost
+
+        ans = dp[0]
         for i in range(1, n):
   
-            last_non_overlap = self.binarySearch(0, i, startTime, endTime)
+            last_non_overlap = bs(0, i)
             if last_non_overlap == -1:
                 dp[i] = value[i]
             else:
                 dp[i] = prev_max_val[last_non_overlap] + value[i]
-                
-            prev_max_val[i] = max(prev_max_val[i-1], value[i])
+            
+            ans = fmax(ans, dp[i])
+            prev_max_val[i] = fmax(prev_max_val[i-1], value[i])
 
-        return max(dp)
+        return ans
     
 events = [[1,3,2],[4,5,2],[2,4,3]]
 events = [[1,3,2],[4,5,2],[1,5,5]]
