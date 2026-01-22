96cf00ab68131c2287781c0aed0490997a4599cf
ChineseResearcher <notneasd@gmail.com>
2026-01-22 16:48:04 +0800

    Improve Q3315

--- bit_manipulation/Q3315 Construct the Minimum Bitwise Array II.py
+++ bit_manipulation/Q3315 Construct the Minimum Bitwise Array II.py
@@ -3,39 +3,32 @@ from typing import List
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
    
-        def get_set_bits(num: int) -> List[int]:
+        # key ideas:
+        # 1) the operation of a OR (a+1) is essentially taking the first
+        # 0-bit to the left of trailing 1-bits and flip it to 1
 
-            # given a 10-based number, record the set
-            # bits' positions in an arr
-
-            idx = 0
-            res = []
-            while (1 << idx) <= num:
-                if num & (1 << idx) != 0:
-                    res.append(idx)
-
-                idx += 1
-
-            return res
+        # 2) for a number to have an answer, i.e. can be formed by some
+        # a OR (a+1), it must have at least 1 trailing 1
+        # note: 0b110110 does not have trailing 1-bit
 
         ans = []
-        for num in nums:
+        for x in nums:
 
-            sb = get_set_bits(num)
-            # create a mask to explore potential candidates
-            # s.t. mask | (mask - 1) = num
-            mask = 0
+            if x & (1 << 0) == 0:
+                ans.append(-1)
+                continue
 
-            curr_ans = float('inf')
-            for i in range(len(sb)-1, -1, -1):
-                
-                mask |= (1 << sb[i])
-                # mask explored will be from smaller to bigger
-                # as we are including more set bits in the left common region
-                if (mask | (mask - 1)) == num:
-                    curr_ans = min(curr_ans, mask - 1) # we need to minimise ans[i]
+            idx = 0
+            while True:
 
-            ans.append(curr_ans if curr_ans < float('inf') else -1)
+                # locate the first 0-bit to the left
+                # of trailing 1-bit(s), and unset the idx-1 bit
+                if x & (1 << idx) == 0:
+                    x &= ~(1 << (idx-1))
+                    break
+                idx += 1
+
+            ans.append(x)
 
         return ans
     
