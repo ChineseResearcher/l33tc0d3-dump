2c88a68cb2f2de7cb27c3de4369b267426308cd1
ChineseResearcher <notneasd@gmail.com>
2026-04-02 20:09:17 +0800

    Improve Q874

--- simulation/Q874 Walking Robot Simulation.py
+++ simulation/Q874 Walking Robot Simulation.py
@@ -1,52 +1,40 @@
 # simulation - medium
+from typing import List
 class Solution:
-    def checkIfBlocked(self, body_position, curr_location, distance_to_travel):
-        if body_position in [0, 2]:
-            for i in range(1, distance_to_travel+1):
-                sim_location = [curr_location[0], curr_location[1]+i if body_position == 0 else curr_location[1]-i]
-                if tuple(sim_location) in self.obstacles: # O(1) check in hashset
-                    return (True, sim_location)
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
 
-        if body_position in [1, 3]:
-            for i in range(1, distance_to_travel+1):
-                sim_location = [curr_location[0]+i if body_position == 1 else curr_location[0]-i, curr_location[1]]
-                if tuple(sim_location) in self.obstacles:
-                    return (True, sim_location)
+        fmax = lambda a, b: a if a > b else b
+        # define useful constants
+        body_pos = [0,1,2,3] # north, east, south, west
+        delta = [(0,1),(1,0),(0,-1),(-1,0)]
 
-        return (False, [])
+        # use a hashset to record obstacles
+        b = set([(r, c) for r, c in obstacles])
 
-    def robotSim(self, commands, obstacles):
-        self.obstacles = set(tuple(obs) for obs in obstacles) 
-        normal_offset = [[1,1], [0,1], [1,-1], [0,-1]]
-        obstacle_offset = [[1,-1], [0,-1], [1,1], [0,1]]
+        # euc. dist. helper
+        euc_dist = lambda r, c: pow(r, 2) + pow(c, 2)
 
-        body_position = 0 # north-facing
-        curr_location = [0,0]
-        getSquaredEuc = lambda coord: int(sum(x**2 for x in coord))
+        r, c, p, ans = 0, 0, 0, 0
+        for x in commands:
+            # turn left
+            if x == -2:
+                p = (p - 1 + 4) % 4
+                continue
+            # turn right
+            if x == -1:
+                p = (p + 1 + 4) % 4
+                continue
 
-        max_euclidean = 0
-        for c in commands:
-            if c == -1:
-                body_position = (body_position + 1) % 4
-            elif c == -2:
-                body_position = (body_position - 1) % 4
-            else:
-                block_sim = self.checkIfBlocked(body_position, curr_location, c)
-                block_status, block_coord = block_sim[0], block_sim[1]
-                
-                if block_status:
-                    offset_axis = obstacle_offset[body_position][0]
-                    offset = obstacle_offset[body_position][1]
-                    block_coord[offset_axis] += offset
-                    curr_location = block_coord # updating
-                else:
-                    axis = normal_offset[body_position][0]
-                    offset = normal_offset[body_position][1] * c
-                    curr_location[axis] += offset
+            dr, dc = delta[p][0], delta[p][1]
+            for _ in range(x):
+                nr, nc = r + dr, c + dc
+                if (nr, nc) in b:
+                    break
+                # implement the move
+                r, c = nr, nc
+                ans = fmax(ans, euc_dist(r, c))
 
-                max_euclidean = max(max_euclidean, getSquaredEuc(curr_location))
-
-        return max_euclidean
+        return ans
     
 commands, obstacles = [4,-1,3], []
 commands, obstacles = [4,-1,4,-2,4], [[2,4]]
