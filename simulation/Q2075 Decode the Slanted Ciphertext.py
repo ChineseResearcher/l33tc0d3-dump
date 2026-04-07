66623d07054948aaf16a167b6f0bf565ffb74d37
ChineseResearcher <notneasd@gmail.com>
2026-04-07 19:49:47 +0800

    Q2075 Decode the Slanted Ciphertext

---
+++ simulation/Q2075 Decode the Slanted Ciphertext.py
@@ -0,0 +1,23 @@
+# simulation - medium
+import math
+class Solution:
+    def decodeCiphertext(self, s: str, rows: int) -> str:
+        if not s:
+            return s
+
+        cols = math.ceil(len(s) / rows)
+        res = []
+
+        for start in range(cols):
+            r, c = 0, start
+            while r < rows and c < cols:
+                res.append(s[r * cols + c])
+                r += 1
+                c += 1
+
+        return ''.join(res).rstrip()
+
+encodedText, rows = "ch   ie   pr", 3
+encodedText, rows = "iveo    eed   l te   olc", 4
+
+Solution().decodeCiphertext(encodedText, rows)
