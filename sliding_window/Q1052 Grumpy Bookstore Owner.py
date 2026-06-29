40fd24ff07376b6173daa77e2b8fda8e02718533
ChineseResearcher <notneasd@gmail.com>
2026-06-29 23:04:55 +0800

    Improve Q1052

--- sliding_window/Q1052 Grumpy Bookstore Owner.py
+++ sliding_window/Q1052 Grumpy Bookstore Owner.py
@@ -1,34 +1,35 @@
 # sliding window - medium
+from typing import List
 class Solution:
-    def maxSatisfied(self, customers, grumpy, minutes) -> int:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         
-        if len(grumpy) <= minutes or sum(grumpy) == 0:
-            return sum(customers)
+        n = len(customers)
+        fmax = lambda a, b: a if a > b else b
+        # key ideas:
+        # 1) move a fixed-size sliding window with length "minutes"
+        # since the secret technique has to be applied in consecutive blocks
+        # 2) pre-compute prefix sums of satisfaction so as to make
+        # satisfaction accounting more efficient when sliding window
 
-        # first window is just the window (0, minutes)
-        windowSum = sum(customers[:minutes])
-        windowlessSum = sum([v for idx, v in enumerate(customers) if grumpy[idx] == 0])
-        # we need to initialise currSum, which includes removing the double counted happy states (0 states)
-        currSum = windowSum + windowlessSum - sum([customers[idx] for idx, state in enumerate(grumpy[:minutes]) if state == 0])
+        pf_sum = [0] * n
+        if grumpy[0] == 0:
+            pf_sum[0] = customers[0]
 
-        maxSum = currSum
+        for i in range(1, n):
+            pf_sum[i] = pf_sum[i-1] + (customers[i] if grumpy[i] == 0 else 0)
 
-        for left in range(1, len(grumpy)-minutes+1):
+        # slide window
+        windowSum = 0
+        for i in range(minutes):
+            windowSum += customers[i]
 
-            # we check every possible window of size = minutes
-            deleted, added = customers[left-1], customers[left + minutes - 1]
-            currSum = currSum - deleted + added
-            
-            # deal with double counting
-            if grumpy[left-1] == 0:
-                currSum += deleted
-                
-            if grumpy[left + minutes - 1] == 0:
-                currSum -= added
-                
-            maxSum = max(maxSum, currSum)
-            
-        return maxSum
+        ans = pf_sum[-1] - pf_sum[minutes-1] + windowSum
+        for r in range(minutes, n):
+            windowSum += customers[r]
+            windowSum -= customers[r-minutes]
+            ans = fmax(ans, pf_sum[-1] - (pf_sum[r] - pf_sum[r-minutes]) + windowSum)
+
+        return ans
     
 customers, grumpy, minutes = [1,0,1,2,1,1,7,5], [0,1,0,1,0,1,0,1], 3
 customers, grumpy, minutes = [10,1,7], [0,0,0], 2
