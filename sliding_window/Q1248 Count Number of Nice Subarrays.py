899909dfb38e75c02c3ec55eafe73a48c186da5c
ChineseResearcher <notneasd@gmail.com>
2026-06-30 11:26:11 +0800

    Improve Q1248

--- sliding_window/Q1248 Count Number of Nice Subarrays.py
+++ sliding_window/Q1248 Count Number of Nice Subarrays.py
@@ -1,37 +1,47 @@
 # sliding window - medium
+from typing import List
 class Solution:
-    def numberOfSubarrays(self, nums, k) -> int:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
 
-        subarrayOddCount = 0
-        ans = 0
-        leftPointer = 0
         n = len(nums)
-
+        # key ideas:
+        # 1) use a sliding window which tracks odd element count
+        # 2) shrink left bound whenever count exceeds k
+        # 3) when accounting for nice arrays, we are interested in the
+        # index of leftmost odd element in our window [l, r]. Therefore, we need to
+        # pre-compute the next odd element index for every odd element.
+        nextOdd, prev = dict(), -1
         for i in range(n):
-
             if nums[i] % 2 == 1:
-                subarrayOddCount += 1
+                nextOdd[prev] = i
+                prev = i
 
-                if subarrayOddCount == k:
-                    
-                    rightPointer = i + 1
-                    leftCount, rightCount = 1, 1
-                    while nums[leftPointer] % 2 == 0:
-                        leftPointer += 1
-                        leftCount += 1
+        if not nextOdd: return 0
 
-                    while rightPointer < n and nums[rightPointer] % 2 == 0:
-                        rightPointer += 1
-                        rightCount += 1
+        l, l_odd, ans, oddCnt = 0, nextOdd[-1], 0, 0
+        for r in range(n):
 
-                    ans += leftCount * rightCount
-                    subarrayOddCount -= 1
-                    leftPointer += 1
+            if nums[r] % 2 == 1:
+                oddCnt += 1
+
+            # shrink window
+            while oddCnt > k:
+                if nums[l] % 2 == 1:
+                    oddCnt -= 1
+                l += 1
+
+            # move pointer to the leftmost odd element
+            while l_odd < l:
+                l_odd = nextOdd[l_odd]
+
+            # validate nice array
+            if oddCnt == k:
+                ans += (l_odd - l + 1)
 
         return ans
-    
-nums, k = [1,1,2,1,1], 3
+
 nums, k = [2,4,6], 1
+nums, k = [1,1,2,1,1], 3
 nums, k = [2,2,2,1,2,2,1,2,2,2], 2
 
 Solution().numberOfSubarrays(nums, k)
