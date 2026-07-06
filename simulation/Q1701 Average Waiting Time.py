331547c13460c80aff3ad1c0d2cf76866cdd04ad
ChineseResearcher <notneasd@gmail.com>
2026-07-07 00:13:48 +0800

    Improve Q1701

--- simulation/Q1701 Average Waiting Time.py
+++ simulation/Q1701 Average Waiting Time.py
@@ -1,18 +1,21 @@
 # simulation - medium
+from typing import List
 class Solution:
-    def averageWaitingTime(self, customers):
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
         
-        # currJobEnd tracks the i-th minute at which the ongoing work ends
-        # with the default to be the first arrival time
-        currJobEnd = customers[0][0]
-        waitingTime = []
+        n = len(customers)
+        fmax = lambda a, b: a if a > b else b
+        # key ideas:
+        # 1) simulate the sequence of events by tracking the end time
+        # of curr. active order
+        # 2) waiting time for a customer = 
+        # time waited before order is procesed + processing duration
+        et, total = 0, 0
+        for arr, duration in customers:
+            total += fmax(et-arr, 0) + duration
+            et = fmax(et, arr) + duration
 
-        for customer in customers:
-            arrival, process_time = customer[0], customer[1]
-            waitingTime.append(max(currJobEnd - arrival, 0) + process_time)
-            currJobEnd = currJobEnd + process_time if arrival <= currJobEnd else arrival + process_time
-
-        return sum(waitingTime) / len(waitingTime)
+        return total / n
     
 customers = [[1,2],[2,5],[4,3]]
 customers = [[5,2],[5,4],[10,3],[20,1]]
