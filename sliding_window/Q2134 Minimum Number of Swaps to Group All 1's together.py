f0be237839d3e6bb767144e8e80f2cc4835d6f8b
ChineseResearcher <notneasd@gmail.com>
2026-08-05 19:54:59 +0800

    Improve Q2134

--- sliding_window/Q2134 Minimum Number of Swaps to Group All 1's together.py
+++ sliding_window/Q2134 Minimum Number of Swaps to Group All 1's together.py
@@ -1,51 +1,48 @@
 # sliding window - medium
+from typing import List
 class Solution:
-    def minSwaps(self, nums):
+    def minSwaps(self, nums: List[int]) -> int:
 
-        # handle special cases
-        # case 1: all ones - already grouped
-        if nums.count(1) == len(nums):
-            return 0
-        
-        # case 2: only zeroes - no need to group
-        if len(set(nums)) == 1 and set(nums).pop() == 0:
-            return 0
-
-        ones_cnt = nums.count(1)
         n = len(nums)
-        min_moves = float('inf')
+        fmin = lambda a, b: a if a < b else b
+        # key ideas:
+        # 1) we can apply fixed sliding window to solve
+        # 2) let the window length be the count of "1"s in nums, and
+        # enumerate all possible windows, including the circular ones
+        # 3) count of "0"s in every sliding window is the count of swaps needed
 
-        # we construct a rolling window of 1s of size = ones_cnt
-        # note that circular property is allowed
-        # e.g., for ones_cnt = 3, [1, 0, ..., 0, 1, 1] is a valid window of ones grouped
-        
-        # to avoid recounting the zeros in each window of size = ones_cnt
-        # we initiate a variable win_zero_cnt and keep track of it
-        win_zero_cnt = nums[:ones_cnt].count(0)
+        k = nums.count(1)
+        # all-zero case & all-one case: no "1"s to be grouped
+        if k == 0 or k == n: return 0
 
-        # case 3: already grouped in a fashion that is left bound 
-        if win_zero_cnt == 0:
-            return 0
+        o, z = 0, 0
+        for i in range(k):
+            if nums[i] == 1:
+                o += 1
+            else:
+                z += 1
 
-        for left in range(1, n):
-            right = (left + ones_cnt - 1) % n
-            prev_left = left - 1
+        ans = z
+        for l in range(1, n):
+            r = (l + k - 1) % n
+            # adjust in-window counts
+            if nums[r] == 1:
+                o += 1
+            else:
+                z += 1
 
-            # handle the left end
-            if nums[prev_left] == 0:
-                win_zero_cnt -= 1
+            if nums[l - 1] == 1:
+                o -= 1
+            else:
+                z -= 1
 
-            # handle the right end 
-            if nums[right] == 0:
-                win_zero_cnt += 1
-            
-            min_moves = min(min_moves, win_zero_cnt)
+            ans = fmin(ans, z)
 
-        return min_moves
+        return ans
     
+nums = [1,1,0,0]
+nums = [1,1,0,0,1]
 nums = [0,1,0,1,1,0,0]
 nums = [0,1,1,1,0,0,1,1,0]
-nums = [1,1,0,0,1]
-nums = [1,1,0,0]
 
 Solution().minSwaps(nums)
