81679fcc72655f0f4880f25a6d9077258612da49
ChineseResearcher <notneasd@gmail.com>
2026-09-11 00:02:44 +0800

    Improve Q209

--- sliding_window/Q209 Minimum Size Subarray Sum.py
+++ sliding_window/Q209 Minimum Size Subarray Sum.py
@@ -1,36 +1,27 @@
 # sliding window - medium
+from typing import List
 class Solution:
-    def minSubArrayLen(self, target, nums):
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
         n = len(nums)
+        fmin = lambda a, b: a if a < b else b
+        # key ideas:
+        # 1) move a sliding window, and shrink whenever windowSum is at least target
+        # 2) track the min. size of all such shrunk windows
 
-        # handle special case
-        # case 1: nums of size 1
-        if n == 1:
-            return n if nums[0] >= target else 0
+        l, windowSum, ans = 0, 0, n + 1
+        for r in range(n):
+            windowSum += nums[r]
+            while windowSum >= target:
+                ans = fmin(ans, r - l + 1)
+                windowSum -= nums[l]
+                l += 1
 
-        # case 2: first num already satisfies target
-        if nums[0] >= target:
-            return 1
-            
-        l = 0 
-        subArrSum = nums[0]
-        
-        minSubArrLength = float('inf')
-        
-        for r in range(1, n):
-            subArrSum += nums[r]
-        
-            if subArrSum >= target:
-                while subArrSum >= target:
-                    minSubArrLength = min(minSubArrLength, r-l+1)
-                    subArrSum -= nums[l]
-                    l += 1
-
-        return 0 if minSubArrLength == float('inf') else minSubArrLength
+        return ans if ans <= n else 0
     
-target, nums = 7, [2,3,1,2,4,3]
 target, nums = 4, [1,4,4]
-target, nums = 11, [1,1,1,1,1,1,1,1]
 target, nums = 6, [10,2,3]
+target, nums = 7, [2,3,1,2,4,3]
+target, nums = 11, [1,1,1,1,1,1,1,1]
 
 Solution().minSubArrayLen(target, nums)
