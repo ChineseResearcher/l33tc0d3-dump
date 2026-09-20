2e04da65c7de1dfc6ddf6614e65dabeb3d26a0a0
ChineseResearcher <notneasd@gmail.com>
2026-09-20 16:57:00 +0800

    Improve Q322

--- dynamic_programming/Q322 Coin Change.py
+++ dynamic_programming/Q322 Coin Change.py
@@ -1,23 +1,29 @@
 # dp - medium
+from typing import List
 class Solution:
-    def coinChange(self, coins, amount):
-        
-        coinSet = set(coins)
-        dp = [float('inf')] * (amount+1)
+    def coinChange(self, coins: List[int], amount: int) -> int:
 
-        # you can always make up 0 dollar without using any coins
+        fmin = lambda a, b: a if a < b else b
+        # key ideas:
+        # 1) for some amount "a", it is possible to form "a-c" first
+        # and achieve "a" with an additional coin
+        # 2) the fewest coins needed to form "a-c" is stored in dp[a - c]
+        dp = [float('inf')] * (amount + 1)
+
+        # 0 dollar with no coins
         dp[0] = 0
 
-        for i in range(1, amount+1):
-            for coin in coinSet:
-                if i - coin >= 0: 
-                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        all_coins = sorted(set(coins))
+        for a in range(1, amount + 1):
+            for c in all_coins:
+                if c > a: break
+                dp[a] = fmin(dp[a], dp[a - c] + 1)
 
         return dp[amount] if dp[amount] < float('inf') else -1
     
-coins, amount = [1,2,5], 11
 coins, amount = [2], 3
 coins, amount = [1], 0
+coins, amount = [1,2,5], 11
 coins, amount = [156,265,40,280], 9109
 
 Solution().coinChange(coins, amount)
